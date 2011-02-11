f5fb1e7f61a4e68e9cdeddc5bf1a9788badad536
glock91@gmail.com <glock91@gmail.com@fdba229b-cd70-3eb6-4b04-595b04e95c13>
2011-02-11 04:58:57 +0000

    added function to the cancel button

--- OfficeManager/OfficeManager/Forms/formSchedule.Designer.cs
+++ OfficeManager/OfficeManager/Forms/formSchedule.Designer.cs
@@ -90,6 +90,7 @@
             this.btnCancel.TabIndex = 4;
             this.btnCancel.Text = "Cancel";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // btnSave
             // 
--- OfficeManager/OfficeManager/Forms/formSchedule.cs
+++ OfficeManager/OfficeManager/Forms/formSchedule.cs
@@ -153,7 +153,9 @@ namespace OfficeManager.Forms
            
         }
 
-        
-
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
